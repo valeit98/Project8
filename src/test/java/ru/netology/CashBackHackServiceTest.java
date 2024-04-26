34c8715e56a019b8c3bc7832e85b689426bdfd97
Valeriy Dobrov <valeriy.dobrov.98@yandex.ru>
2024-04-26 20:27:35 +0400

    testng realization

--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,21 +1,18 @@
 package ru.netology;
 
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.*;
 
 public class CashBackHackServiceTest {
 
     @Test
     public void ShouldReturn1000() {
         CashBackHackService service = new CashBackHackService();
-
         int amount = 0;
-
-        int expected = 1000;
         int actual = service.remain(amount);
-
+        int expected = 1000;
         assertEquals(expected, actual);
 
     }
@@ -23,12 +20,9 @@ public class CashBackHackServiceTest {
     @Test
     public void ShouldReturn900() {
         CashBackHackService service = new CashBackHackService();
-
         int amount = 100;
-
-        int expected = 900;
         int actual = service.remain(amount);
-
+        int expected = 900;
         assertEquals(expected, actual);
 
     }
@@ -36,15 +30,10 @@ public class CashBackHackServiceTest {
     @Test
     public void ShouldReturn700() {
         CashBackHackService service = new CashBackHackService();
-
         int amount = 1300;
-
-        int expected = 700;
         int actual = service.remain(amount);
-
+        int expected = 700;
         assertEquals(expected, actual);
-
     }
-
 
 }
